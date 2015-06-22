8b050c60eecd17a285798ebe20ba2bff494c333a
Your Name <you@example.com>
2015-06-22 19:22:20 -0400

    Removed AGPL license, will use LGPL license from repo

--- Hermes/Hermes.js
+++ Hermes/Hermes.js
@@ -1,21 +1,4 @@
 /**
- * Copyright 2013 Densaugeo (Nathan Yinger)
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * Affero General Public License for more details.
- * 
- * You should have received a copy of the Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- **
- * 
  * This is essentilly a hackish raster font for html canvases.
  * Characters are 8 pixels wide; lines are 12 pixels high.
  * 
